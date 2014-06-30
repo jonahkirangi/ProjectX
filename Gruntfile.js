67d497b51cad2a6a2b74a50cc073433f5b13722a
Jonah Kirangi <jonahkirangi@gmail.com>
2014-06-29 22:02:48 -0700

    Add Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-simple-mocha');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.initConfig({
+    simplemocha: {
+      options: {
+        timeout: 3000,
+        ignoreLeaks: false,
+        reporter: 'tap'
+      },
+
+      all: { src: ['test/**/*.js'] }
+    },
+
+    jshint: {
+      all: ['Gruntfile.js', 'server.js', 'test/**/*.js'],
+      options: {
+        jshintrc: true,
+        globals: {
+          console: true,
+          module: true
+        }
+      }
+    },
+  });
+
+  grunt.registerTask('default', ['simplemocha', 'jshint']);
+};
