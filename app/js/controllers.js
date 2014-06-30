1bb3c7822ee4e3a436e1a21ca3984bf73928b6f9
Jonah Kirangi <jonahkirangi@gmail.com>
2014-06-29 19:05:22 -0700

    Add job filter search

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,6 +1,41 @@
 var projectXcontrollers = angular.module('projectXcontrollers', []);
 
-projectXcontrollers.controller('ProjectXCtrl', function($scope){
-  $scope.test = 'Data binding is working';
-  $scope.broken = 15;
+projectXcontrollers.controller('JobListCtrl', function($scope){
+    $scope.jobs = [
+      {
+        id: 0,
+        job_title: 'Database Analyst',
+        company_name: 'Amazon',
+        location: 'Seattle',
+        date_applied: 'April 12, 2014'
+      },
+      {
+        id: 1,
+        job_title: 'Front End Dev',
+        company_name: 'Redfin',
+        location: 'Seattle',
+        date_applied: 'May 1, 2014'
+      },
+      {
+        id: 2,
+        job_title: 'Back End Dev',
+        company_name: 'Rover',
+        location: 'Seattle',
+        date_applied: 'June 15, 2014'
+      },
+      {
+        id: 3,
+        job_title: 'Full Stack Dev',
+        company_name: 'Wet Paint',
+        location: 'Seattle',
+        date_applied: 'June 8, 2014'
+      },
+      {
+        id: 4,
+        job_title: 'Design Wizard',
+        company_name: 'Socrata',
+        location: 'Seattle',
+        date_applied: 'April 2, 2014'
+      }
+    ];
   });
