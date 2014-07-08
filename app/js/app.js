944fe7b3f8cf2da0066bc9cc5ea0c8ff7ac68ea5
Jonah Kirangi <jonahkirangi@gmail.com>
2014-07-08 11:35:12 -0700

    Add angular routing and views

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,22 @@
+'use strict';
+
 var projectXapp = angular.module('projectXapp', [
+  'ngRoute',
   'projectXcontrollers'
 ]);
+
+projectXapp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'partials/job-list.html',
+        controller: 'JobListCtrl'
+      }).
+      when('/addjob', {
+        templateUrl: 'partials/add-job.html',
+        controller: 'JobListCtrl'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+  }]);
