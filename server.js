462308bb1f1f8071628a87698c6d1947497687bf
Jonah Kirangi <jonahkirangi@gmail.com>
2014-07-08 20:09:06 -0700

    Modify app routing

--- server.js
+++ server.js
@@ -3,12 +3,21 @@
 var express = require('express');
 var http    = require('http');
 var mongoose = require('mongoose');
+var fs = require('fs');
 
 var app = express();
 
 var db = mongoose.createConnection('localhost', 'projectx');
 
+var index;
+fs.readFile(__dirname + '/app/index.html', function(error, content) {
+  if(error) throw error;
+  index = content;
+});
 app.use(express.static(__dirname + '/app'));
+app.use(function(request, response) {
+  response.end(index);
+});
 
 var server = http.createServer(app);
 app.set('port', process.env.PORT || 3000);
